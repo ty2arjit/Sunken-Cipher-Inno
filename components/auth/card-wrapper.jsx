420e87a86e65d97c13eedf8471d87f96d3e59ea9
ty2arjit <ty2arjit@gmail.com>
2025-11-04 12:35:52 +0530

    Register form colour changed to teal

--- components/auth/card-wrapper.jsx
+++ components/auth/card-wrapper.jsx
@@ -6,7 +6,7 @@ import { Header } from "./header";
     headerLabel,
   }) => {
     return (
-      <Card className="w-[35%] shadow-md">
+      <Card className="w-[35%]  bg-slate-900 text-slate-50 rounded-lg shadow-lg shadow-cyan-600/60">
         <CardHeader >
             <Header label={headerLabel} />
         </CardHeader>
