e1e21e65722b9606676627b92159824b071f26a4
ty2arjit <ty2arjit@gmail.com>
2025-11-04 12:37:28 +0530

    registration page

--- app/(auth)/register/page.jsx
+++ app/(auth)/register/page.jsx
@@ -3,9 +3,9 @@ import { RegisterForm } from '@/components/auth/register-form'
 import { Instructions } from '@/components/auth/Instructions'
 const RegisterPage = () => {
   return ( 
-      <div className="flex justify-center w-[90%]  rounded-lg space-x-8 bg-opacity-95 ">
+      <div className="flex justify-center w-[90%] rounded-lg space-x-8 bg-opacity-95 ">
         <Instructions />
-        <RegisterForm />
+        <RegisterForm /> 
       </div>
     
   );
