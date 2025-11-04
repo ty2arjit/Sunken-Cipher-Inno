b86289657e620e84883ed49d467d83c839819b8e
ty2arjit <ty2arjit@gmail.com>
2025-11-04 12:42:41 +0530

    Hero page with landing page animation

--- components/main/Hero.jsx
+++ components/main/Hero.jsx
@@ -11,9 +11,9 @@ const Hero = () => {
         autoPlay
         muted
         loop
-        className="rotate-180 absolute top-[-340px]  h-full w-full left-0 z-[1] object-cover "
+        className="absolute top-10 h-screen w-full left-0 z-[1] object-cover "
       >
-        <source src="/blackhole.webm" type="video/webm" />
+        <source src="/Final_landing_page.mp4" type="video/mp4" />
       </video>
       <HeroContent />
 
