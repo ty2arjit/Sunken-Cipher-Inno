e894d2cc9ff748a214fe3e8e86c3a641a37bc891
ty2arjit <ty2arjit@gmail.com>
2025-11-06 14:58:12 +0530

    Fix : footer

--- components/sub/HeroContent.jsx
+++ components/sub/HeroContent.jsx
@@ -21,16 +21,19 @@ const HeroContent = () => {
 
                 <motion.div
                     variants={slideInFromLeft(0.5)}
-                    className="flex flex-col gap-6 mt-3 text-5xl font-bold text-white max-w-[600px] w-auto h-auto"
+                    className="flex flex-col gap-6 mt-3 text-8xl font-bold text-white max-w-[600px] w-auto h-auto"
                 >
                     <h1
-                        className="mb-4 text-8xl leading-none tracking-tight text-white"
-                        style={{ letterSpacing: 2, fontFamily: "myFirstFont" }}
+                        className="mb-4 text-256xl tracking-tight text-white relative top-20"
+                        style={{ letterSpacing: 2, 
+                        fontFamily: "myFirstFont" ,
+                        
+                        }}
                     >
                         SUNKEN <br /> CIPHER
                     </h1>
                     <p
-                        className="mb-6 text-2xl font-normal text-white-500 dark:text-gray-300 text-left"
+                        className="mb-6 text-4xl font-normal text-white-500 dark:text-gray-300 text-left relative top-20"
                         style={{ fontFamily: "Roboto" }}
                     >
                         Let us commence the APS Game of SunkenCipher.
@@ -42,9 +45,9 @@ const HeroContent = () => {
                 <motion.a
                     variants={slideInFromLeft(2,2)}
                     onClick={handleRegisterClick}
-                    className="py-2 px-[4rem] -ml-6 flex items-center mt-75 justify-center text-center text-white text-2xl cursor-pointer rounded-lg  h-16
+                    className="py-2 px-[4rem] -ml-6 flex items-center mt-100 justify-center text-center text-white text-2xl cursor-pointer rounded-lg  h-16
                bg-cyan-500 hover:bg-cyan-900 shadow-lg shadow-indigo-900/30
-               border border-transparent hover:border-blue-900 transition duration-300 ease-in-out transform hover:scale-105 "
+               border border-transparent hover:border-blue-900 transition duration-300 ease-in-out transform hover:scale-105 relative top-20 -left-10 "
                 >
                     Register
                 </motion.a>
