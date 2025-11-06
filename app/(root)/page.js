98d88dc2d768693f794963b5682f275bf5215c0a
ty2arjit <ty2arjit@gmail.com>
2025-11-06 14:58:52 +0530

    Fix: footer UI

--- app/(root)/page.js
+++ app/(root)/page.js
@@ -8,13 +8,10 @@ export default async function Home() {
     </div>
     <div className="w-full text-center text-white p-6 mt-8 relative z-[10]">
   <div className="flex flex-col items-center space-y-2">
-    <div className="text-2xl font-semibold bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 text-transparent bg-clip-text">
-      Made by APS with ❤
-    </div>
-    <div className="flex space-x-2">
-      <span className="text-blue-400 animate-pulse">🚀</span>
-      <span className="text-purple-400 animate-pulse">🌌</span>
-      <span className="text-pink-400 animate-pulse">✨</span>
+    <div className="text-2xl font-semibold bg-gradient-to-r from-blue-100 via-cyan-300 to-blue-200 text-transparent bg-clip-text text-yellow-500" 
+    style = {{ marginTop: '200px' }}
+    >
+      Made by APS with <span style={{color: "red"}}>❤</span>
     </div>
   </div>
   <div className="absolute inset-0 rounded-md bg-gradient-to-r from-blue-800 via-purple-800 to-pink-800 opacity-25 blur-lg"></div>
