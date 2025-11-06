3c7064c11fade0a63eae4bd532d43f60fb95b73b
ty2arjit <ty2arjit@gmail.com>
2025-11-06 14:59:21 +0530

    features : form error and form success

--- components/form-error.jsx
+++ components/form-error.jsx
@@ -8,7 +8,7 @@ export const FormError = ({
   return (
     <div className="bg-destructive/15 p-3 rounded-md flex items-center gap-x-2 text-sm text-destructive">
       <ExclamationTriangleIcon className="h-4 w-4" />
-      <p>{message}</p>
+      <p>{ message }</p>
     </div>
   );
 };
